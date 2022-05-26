aefbc0200c1250bbbb68a1102bbe07eeb77405e6
abidasultana-code <aisayam.sayam@gmail.com>
2022-05-27 01:58:05 +0600

    sidebar layout, software and graphics services completed

---
+++ resources/views/frontend/layouts/sidebar.blade.php
@@ -0,0 +1,42 @@
+<div class="sidebar-side left-sidebar col-lg-4 col-md-12 col-sm-12">
+    <aside class="sidebar sticky-top">
+
+        <!-- Services -->
+        <div class="sidebar-widget">
+            <ul class="service-list">
+                <li><a href="{{url('/services')}}"><span class="color-layer"></span>All Services</a></li>
+                <li><a href="{{url('/services/software_solutions')}}"><span class="color-layer"></span>Software Solutions</a></li>
+                <li class="current"><a href="{{url('/services/graphics_design')}}"><span class="color-layer"></span>Graphics Design</a></li>
+                <li><a href="{{url('/services/functional_resume')}}"><span class="color-layer"></span>Functional CV</a></li>
+                <li><a href="{{url('/services/brand_identity')}}"><span class="color-layer"></span>Brand Identity</a></li>
+                <li><a href="{{url('/services/ui_ux_design')}}"><span class="color-layer"></span>UI/UX Design</a></li>
+                <li><a href="{{url('/services/social_media_marketing')}}"><span class="color-layer"></span>Social Media Marketing</a></li>
+            </ul>
+        </div>
+
+        <!-- Broucher Widget -->
+        <div class="broucher-widget">
+            <div class="widget-content" >
+                <h3>What To Know More About Us</h3>
+
+                <div class="text">Download Our Company Profile</div>
+                <a class="download"> Download </a>
+
+            </div>
+        </div>
+
+        <!-- Help Widget -->
+        <div class="help-widget">
+            <div class="widget-content">
+            <h4>Want To See More ?</h4>
+            <a class="see-work-wrap" href="{{url('/projects')}}">
+                <div class="see-work">
+                    <div class="see-arrow"><i class="fa fa-arrow-right"></i></div>
+                    <div class="see-work-link">View Our Work</div>
+                </div>
+            </a>
+            </div>
+        </div>
+
+    </aside>
+</div>
